955c31641540c3f960119b1d079caf8ae48bef3e
ltees <135617476+ltees@users.noreply.github.com>
2023-08-10 11:00:03 -0300

    Add files via upload

---
+++ Projectc.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main(){
+      
+      //armazenar texto em variaveis 
+      //declaracao
+      char nome[50] = "Maria da Silva";
+      char nomeUsuario[25];
+      char endereco[50];
+
+      printf("O nome digitado é: %s", nome);
+
+      //recebemdo valores do teclado
+
+      printf("\n digite o nome do usuario: ");
+      scanf("%[^\n]", nomeUsuario);
+      printf("Nome Usuario: %s", nomeUsuario);
+      //limpar o buffer do teclado
+      fflush(stdin);
+
+      //entrada de texto usando fgets
+      printf("\n\nDigite o endereco");
+      fgets(endereco, 50, stdin);
+      printf("\n O endereco e; %s", endereco);
+
+}
